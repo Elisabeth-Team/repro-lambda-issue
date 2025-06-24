7f17cdf592de9535b353e6257a88c868aecbbafc
Elisabeth Lichtie <elisabethlichtie99@gmail.com>
2025-06-24 15:31:50 -0500

    see if filearchive is the issue

--- __main__.py
+++ __main__.py
@@ -5,7 +5,16 @@ schemas = ["hi.txt", "what.txt"]
 
 config = pulumi.Config()
 env = config.get("project:environment")
-print(env)
+
+asset = pulumi.asset.AssetArchive({
+           # '.': pulumi.asset.FileArchive('./lambdas/worker'),
+            'config.yml': pulumi.asset.FileAsset(f'./configs/config.yml'),
+            ** {
+                f'schemas/{file_name}': pulumi.asset.FileAsset(f'./schemas/{file_name}')
+                for file_name in schemas
+            }
+        })
+
 worker = aws.lambda_.Function(
     resource_name='worker',
     args=aws.lambda_.FunctionArgs(
@@ -13,13 +22,6 @@ worker = aws.lambda_.Function(
         runtime='python3.9',
         role="arn:aws:iam::052848974346:role/delete-me-elisabeth", # from elsewhere
         handler='lambda_function.lambda_handler2',
-        code=pulumi.asset.AssetArchive({
-            '.': pulumi.asset.FileArchive('./lambdas/worker'),
-            'config.yml': pulumi.asset.FileAsset(f'./configs/config.yml'),
-            ** {
-                f'schemas/{file_name}': pulumi.asset.FileAsset(f'./schemas/{file_name}')
-                for file_name in schemas
-            }
-        })
+        source_code_hash=asset
     )
 )
