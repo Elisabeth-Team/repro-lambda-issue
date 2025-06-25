6f8f369f8b79279f574c77792f4c6ddb50d1d2db
Elisabeth Lichtie <elisabethlichtie99@gmail.com>
2025-06-25 14:56:32 -0500

    return to normal repro

--- __main__.py
+++ __main__.py
@@ -1,23 +1,35 @@
 import pulumi
 import pulumi_aws as aws
-import pulumi_archive as archive
 
 schemas = ["hi.txt", "what.txt"]
+
 config = pulumi.Config()
 env = config.get("project:environment")
 
-lambda_ = archive.get_file(type="zip",
-    source_file="./lambdas/worker/index.py",
-    output_path="lambda_function_payload.zip")
+asset = pulumi.asset.AssetArchive({
+           '.': pulumi.asset.FileArchive('./lambdas/worker'),
+            'config.yml': pulumi.asset.FileAsset(f'./configs/config.yml'),
+            ** {
+                f'schemas/{file_name}': pulumi.asset.FileAsset(f'./schemas/{file_name}')
+                for file_name in schemas
+            }
+        })
 
-worker_manual = aws.lambda_.Function(
-    resource_name='worker-manual',
+hash = asset.__hash__()
+
+print(hash)
+worker = aws.lambda_.Function(
+    resource_name='worker',
     args=aws.lambda_.FunctionArgs(
-        name='worker-manual',
+        name='worker',
         runtime='python3.9',
-        role="arn:aws:iam::052848974346:role/delete-me-elisabeth",
+        role="arn:aws:iam::052848974346:role/delete-me-elisabeth", # from elsewhere
         handler='lambda_function.lambda_handler2',
-        source_code_hash=lambda_.output_base64sha256,
-        code=pulumi.FileArchive("lambda_function_payload.zip"),
+        source_code_hash="WFYpNzO6uoMXQ6TaqRGAoqgsbINfd6gBykzu+3k0wRI=",
+        code=asset
     )
 )
+
+pulumi.export("hash1", worker.source_code_hash)
+
+pulumi.export("hash", hash)
